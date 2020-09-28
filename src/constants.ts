85463b030f4fd2364d9f80e41d37b5fe5b83d5e9
Ivan Poon <ravenxce@gmail.com>
2020-09-28 12:04:43 +0800

    Add mainnet contract

--- src/constants.ts
+++ src/constants.ts
@@ -17,17 +17,19 @@ export const WSS: { [key in Networks]: string } = {
 }
 
 export const CONTRACTS: { [key in Networks]: string } = {
-  [Network.MainNet]: '',
+  [Network.MainNet]: 'zil14k98ef9ydnve8dc0le3fql2fpe4hap6m887c7k',
   [Network.TestNet]: 'zil1rf3dm8yykryffr94rlrxfws58earfxzu5lw792',
 }
 
 export const TOKENS: { [key in Networks]: { [key2 in string]: string } } = {
   [Network.MainNet]: {
     ZIL: 'zil1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq9yf6pz',
+    XSGD: 'zil1p7qk0gxtl7u2k8ger833lq7uymyx858eay0w7t', // Xfers SGD
   },
   [Network.TestNet]: {
     ZIL: 'zil1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq9yf6pz',
     SWTH: 'zil1d6yfgycu9ythxy037hkt3phc3jf7h6rfzuft0s', // Switcheo Test Token
+    gZIL: 'zil10w2fwf5kdwqvjd2zyv6nr7da2d2z63g5dvtvsy', // ZIL Governance
     XSGD: 'zil10a9z324aunx2qj64984vke93gjdnzlnl5exygv', // Xfers SGD
   },
 }
